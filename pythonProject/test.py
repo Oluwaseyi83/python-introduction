c94419f39ea561e53397c35b0fbd86c2f40dbc92
Oluwaseyi Oyadeyi <oyasey83@gmai.com>
2024-02-17 13:58:03 -0700

    updated script

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -2,11 +2,24 @@ import boto3
 
 client = boto3.client ('ec2')
 
-response = client.run_instance (
+response = client.run_instances(
     ImageId = 'ami-0e731c8a588258d0d',
     InstanceType = 't2.micro',
     MaxCount = 1,
-    MinCount = 1
-)    
+    MinCount = 1,
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'PYHON EC2'
+                },
+            ]
+        },
+    ],
+)
 
 print(response)
+
+
